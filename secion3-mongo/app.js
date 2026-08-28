9109d3513d5a67614386f02cf60a7df3ce10eb33
DeniseLH1 <jdlhprincesa@gmail.com>
2026-08-28 12:09:53 -0600

    feat: nuevas funciones

--- secion3-mongo/app.js
+++ secion3-mongo/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 //paquete para trabajar con los formularios de html
-// import bodyParser from 'body-parser';
+import bodyParser from 'body-parser';
 
 //variable de entorno 
 import 'dotenv/config';
@@ -11,6 +11,8 @@ const app= express();
 
 // indica que usaremos un midleware
 app.use(express.json());
+
+app.use()
 
 //para sevir archivos estaticos usando el siguiente midlleware la cual dira la ubicacion de la carpeta y luego la carpeta exacta en este caso public
 app.use(express.static(`${import.meta.dirname}/public`))
